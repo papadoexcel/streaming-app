1fc9b6754bc86bf94914fb3356541716e2649538
Rodrigo <rlpadilha@gmail.com>
2025-03-28 21:32:14 -0300

    Atualização com suporte a múltiplos dispositivos + painel do operador

---
+++ ffmpeg-receiver.js
@@ -0,0 +1,34 @@
+// === ffmpeg-receiver.js ===
+
+const http = require('http');
+const { spawn } = require('child_process');
+
+const YOUTUBE_URL = 'rtmp://a.rtmp.youtube.com/live2';
+const STREAM_KEY = '550u-qa5d-a0xv-gthd-f1qa'; // substitua por sua chave real
+
+setInterval(() => {
+  http.get('http://localhost:3000/buffer', res => {
+    if (res.statusCode === 200) {
+      const ffmpeg = spawn('ffmpeg', [
+        '-re',
+        '-i', '-',
+        '-c:v', 'libx264',
+        '-preset', 'veryfast',
+        '-tune', 'zerolatency',
+        '-c:a', 'aac',
+        '-b:a', '128k',
+        '-f', 'flv', `${YOUTUBE_URL}/${STREAM_KEY}`
+      ]);
+
+      res.pipe(ffmpeg.stdin);
+
+      ffmpeg.stderr.on('data', data => {
+        console.log('[FFmpeg]', data.toString());
+      });
+
+      ffmpeg.on('close', code => {
+        console.log('FFmpeg finalizado com código', code);
+      });
+    }
+  });
+}, 3000);
